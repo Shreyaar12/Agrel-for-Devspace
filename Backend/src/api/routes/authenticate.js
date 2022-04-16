880e4f7188169af63f4e216a2dbaac7dc8993dd6
Anuj Parihar <bear@tamako.tech>
2022-04-16 19:33:10 +0530

    signup fix

--- Backend/src/api/routes/authenticate.js
+++ Backend/src/api/routes/authenticate.js
@@ -16,11 +16,11 @@ router.post("/signup",
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) { return res.status(400).json({ errors: errors.array() }); }
-    const { email, password, name } = req.body;
+    const { email, password, name, isFarmer } = req.body;
     try {
       let user = await User.findOne({ email });
       if (user) { return res.status(400).json({ message: "User already exists" }); }
-      user = new User({ name, email, password });
+      user = new User({ name, email, password, isFarmer });
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
       await user.save();
